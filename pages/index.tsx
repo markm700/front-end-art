aa22aed66f5f214e1c6f44a373aa938067c4d049
Mark Matas <markmatas@marks-mbp.home>
2021-09-15 14:30:34 -0600

    new image component via nextjs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 import Show from '../comps/shows'
 import styles from '../styles/Home.module.css'
 
@@ -8,7 +9,11 @@ const Home: NextPage = () => {
   return (
     <div>
       <div className={styles.containerleft}>
-        <img className={styles.imgleft} src="/wwelogo.jpeg"/>
+        <Image
+          src="/wwelogo.jpeg"
+          width={100}
+          height={850}
+        />
       </div>
       <div className={styles.container}>
         <Head>
@@ -17,8 +22,12 @@ const Home: NextPage = () => {
         <Show name={"WWE NXT"} titles={nxtTitles} rosterSize={2} />
       </div>
       <div className={styles.containerright}>
-        <img className={styles.imgright} src="/aewlogo.png"/>
-      </div>
+        <Image
+          src="/aewlogo.png"
+          width={200}
+          height={850}
+        />
+      </div>   
     </div>
   )
 }
